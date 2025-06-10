fdd53c56dde16410f97f72841baee21841747e97
javi <elbuenjavizarate@gmail.com>
2025-06-10 10:23:46 -0600

    nuevo admin

--- orientador/admin.py
+++ orientador/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Orientador  # importa el modelo
+from .models import Orientador, EntornoVR  # importa el modelo
 
 admin.site.register(Orientador)
+
+admin.site.register(EntornoVR)
